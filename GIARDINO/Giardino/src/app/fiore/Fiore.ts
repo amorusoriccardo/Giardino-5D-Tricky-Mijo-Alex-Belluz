3515949df7af70135673938efb48c09de16f75dc
ArmijoAlessio <armijo.ramirez.alessio.michele@iisgalvanimi.edu.it>
2020-05-11 19:55:10 +0000

    form fiore e funzione addFiore

---
+++ GIARDINO/Giardino/src/app/fiore/Fiore.ts
@@ -0,0 +1,17 @@
+export class Fiore
+{
+    Specie: string;
+    Altezza : string;
+    Colore : string;
+    Periodo: string;
+
+
+
+    constructor(Specie:string, Altezza: string, Colore: string, Periodo: string){
+        this.Specie = Specie;
+        this.Altezza = Altezza;
+        this.Colore = Colore;
+        this.Periodo = Periodo;
+
+    }
+}
--- GIARDINO/Giardino/src/app/fiore/fiore.component.ts
+++ GIARDINO/Giardino/src/app/fiore/fiore.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Fiore } from './Fiore';
 
 @Component({
   selector: 'app-fiore',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fiore.component.css']
 })
 export class FioreComponent implements OnInit {
+  Fiori : Fiore[];
+  Specie : string;
+  Altezza : string;
+  Colore : string;
+  Periodo: string;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+addFiore(newSpecie: HTMLInputElement, newAltezza: HTMLInputElement, newColore: HTMLInputElement, newPeriodo: HTMLInputElement): boolean {
+  let newFiori: Fiore = new Fiore(this.Specie,this.Altezza,this.Colore,this.Periodo);
+      newFiori.Specie = newSpecie.value;
+      newFiori.Altezza = newAltezza.value;
+      newFiori.Colore = newColore.value;
+      newFiori.Periodo = newPeriodo.value;
 
+
+
+      return false;
+  }
 }
