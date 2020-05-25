b34e5f7cfc2b6d885552e129b96bf96cc3007f0d
ArmijoAlessio <armijo.ramirez.alessio.michele@iisgalvanimi.edu.it>
2020-05-25 22:21:35 +0000

    lista fiori

--- GIARDINO/Giardino/src/app/fiore/fiore.component.ts
+++ GIARDINO/Giardino/src/app/fiore/fiore.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Fiore } from './Fiore';
 
 @Component({
@@ -7,6 +9,9 @@ import { Fiore } from './Fiore';
   styleUrls: ['./fiore.component.css']
 })
 export class FioreComponent implements OnInit {
+   obsFiore: Observable<Fiore[]>;
+  postObserver : Observable<Object>;
+  postData : Object;
   Fiori : Fiore[];
   Specie : string;
   Altezza : string;
@@ -14,9 +19,16 @@ export class FioreComponent implements OnInit {
   Periodo: string;
 
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+   getFioriList(): void {
+    //Qui va sostituito l’url con quello delle vostre api
+    this.obsFiore = this.http.get<Fiore[]>('https://jsonplaceholder.typicode.com/users');
+    //Mi sottoscrivo all’observable e scrivo la arrow function che riceve i dati
+    this.obsFiore.subscribe((data: Fiore[]) => {this.Fiori = data;});
+  }
 
   ngOnInit(): void {
+     this.getFioriList();
   }
 addFiore(newSpecie: HTMLInputElement, newAltezza: HTMLInputElement, newColore: HTMLInputElement, newPeriodo: HTMLInputElement): boolean {
   let newFiori: Fiore = new Fiore(this.Specie,this.Altezza,this.Colore,this.Periodo);
@@ -25,8 +37,13 @@ addFiore(newSpecie: HTMLInputElement, newAltezza: HTMLInputElement, newColore: H
       newFiori.Colore = newColore.value;
       newFiori.Periodo = newPeriodo.value;
 
+      let headers =  {headers: new HttpHeaders().set('Content-Type', 'application/json')};
 
+
+    this.postObserver = this.http.post('http://localhost:3000/users', newFiori);
+    this.postObserver.subscribe(data => this.postData = data);
 
       return false;
+
   }
 }
